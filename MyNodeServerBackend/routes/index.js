52cdc319f7abaa0a9d3cf474b6bb1905a7be6e19
Zachary Schafer <schafezp@rose-hulman.edu>
2016-04-27 14:27:59 -0400

    All database is now created automatically

--- MyNodeServerBackend/routes/index.js
+++ MyNodeServerBackend/routes/index.js
@@ -2,17 +2,35 @@ var express = require('express');
 var router = express.Router();
 var sqlquery = require('../sqlquery.js');
 console.log("Sql query run");
-sqlquery.destroyDatabase(function(){
-    sqlquery.createDatabase(function(){
-        sqlquery.populateDatabase(function(){
-            //sqlquery.queryDatabase();
-            sqlquery.findUserByUsername("schafezp",function(err,data){
-                console.log(data);
+var destroyAndPopulate = function(){
+    sqlquery.destroyDatabase(function(){
+        sqlquery.createDatabase(function(){
+            sqlquery.populateDatabase(function(){
+                //sqlquery.queryDatabase();
+                sqlquery.findUserByUsername("schafezp",function(err,data){
+                    console.log(data);
+                });
             });
         });
     });
-});
+};
+var destroy = function(){
+    sqlquery.destroyDatabase(function(){
+    });
+}
+var destroyAndCreate = function(){
+    sqlquery.destroyDatabase(function(){
+        sqlquery.createDatabase(function(){
 
+        });
+    });
+}
+
+//Leave just destroy to clear the db and start fresh
+//destroyAndCreate();
+destroyAndPopulate();
+//Use destroyAndCreate to destroy and repopulate the database.
+//destroyAndCreate();
 
 /* GET home page. */
 /*
