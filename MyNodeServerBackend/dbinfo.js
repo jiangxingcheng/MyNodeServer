d8d9a72d273ade2c7470be798927ca9361889a2a
xxxperimental <zamanmm@rose-hulman.edu>
2016-04-27 15:59:32 -0400

    Testing this

--- MyNodeServerBackend/dbinfo.js
+++ MyNodeServerBackend/dbinfo.js
@@ -1,4 +1,4 @@
 var massive = require("massive");
-var connectionString = "postgres://dbusername:dbpassword@ipaddress:port/dbname";
+var connectionString = "postgres://nodeserver:change@localhost:5432/mynodeserver";
 var massiveInstance = massive.connectSync({connectionString : connectionString});
 exports.dbInstance = massiveInstance;
