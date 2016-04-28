bb74e50ee4dae6519260c70518eb34f3b949d97b
Zachary Schafer <schafezp@rose-hulman.edu>
2016-04-28 01:36:24 -0400

    Fix Navbar link forom forum. Add forum controller to index.html

--- MyNodeServerFrontend/src/js/controllers/ForumCtrl.js
+++ MyNodeServerFrontend/src/js/controllers/ForumCtrl.js
@@ -1,5 +1,4 @@
 app.controller('ForumCtrl', ['$scope','$location','$log','sqlService', function ($scope,$location, $log,sqlService)
 {
-    $scnope
     //$scope.elements = Useraccount.init();
 }]);
