ff10ac8c4335fea013cb513fd6864a23390e7b6b
Zachary Schafer <schafezp@rose-hulman.edu>
2016-04-28 22:49:08 -0400

    Make user search well formatted

--- MyNodeServerFrontend/src/js/controllers/UserCtrl.js
+++ MyNodeServerFrontend/src/js/controllers/UserCtrl.js
@@ -1,29 +1,19 @@
 app.controller('UserCtrl', ['$scope', '$location', '$log', 'sqlService','$timeout', function ($scope, $location, $log, sqlService,$timeout) {
     $scope.searchuser = "";
     $scope.user = "";
+    $scope.users = [];
     $scope.onClick = function () {
-        sqlService.getUserByUsername($scope.searchuser, function (data) {
-            $scope.user = data;
-        });
-    };
-    $scope.myInterval = 3000;
-    $scope.slides = [
-        {
-            image: 'http://lorempixel.com/400/200/'
-        },
-        {
-            image: 'http://lorempixel.com/400/200/food'
-        },
-        {
-            image: 'http://lorempixel.com/400/200/sports'
-        },
-        {
-            image: 'http://lorempixel.com/400/200/people'
+        $scope.users = [];
+        if($scope.searchuser != ""){//sending no parameter means all users and we only want one
+            sqlService.getUserByUsername($scope.searchuser, function (data) {
+                $scope.users = data;
+                $scope.user = data[0];
+            });
         }
-    ];
-    $scope.users = sqlService.users;
+
+    };
+    $scope.allUsers = sqlService.users;
     sqlService.getUsers(function(data){
-        $scope.users = data;
+        $scope.allUsers = data;
     });
-    $scope.count = 0;
 }]);
