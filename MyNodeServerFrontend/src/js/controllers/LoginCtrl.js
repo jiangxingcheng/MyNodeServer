b287b2c99030c0e25415132c05b12c2a8f852bc8
Zachary Schafer <schafezp@rose-hulman.edu>
2016-05-03 22:42:20 -0400

    Make login work by checking the user exists in the db

--- MyNodeServerFrontend/src/js/controllers/LoginCtrl.js
+++ MyNodeServerFrontend/src/js/controllers/LoginCtrl.js
@@ -1,24 +1,25 @@
 app.controller('LoginCtrl', ['$scope', '$log', 'loginService','$location', function ($scope, $log, loginService,$location) {
     var self = this;
-
     $scope.vm = {
         dataLoading : false,
         loginAttempted : false,
         login : function(){
             var username = $scope.vm.username;
             var password = $scope.vm.password;
-            if(loginService.isAuthenticated(username,password,'')){
-                $log.log('Is Authenticated');
-                window.scrollTo(0,0);
-                $location.url('/home');
-            }else{
-                $log.log('Is Not Authenticated');
-                $scope.vm.loginAttempted = true;
-            };
-            $scope.vm.username = '';
-            $scope.vm.password = '';
-
+            $scope.vm.dataLoading = true;
+            loginService.isAuthenticated(username,password,'',function(isAuthenticated){
+                if(isAuthenticated){
+                    $log.log('Is Authenticated');
+                    window.scrollTo(0,0);
+                    $location.url('/home');
+                }else{
+                    $log.log('Is Not Authenticated');
+                    $scope.vm.loginAttempted = true;
+                }
+                $scope.vm.dataLoading = false;
+                $scope.vm.username = '';
+                $scope.vm.password = '';
+            });
         }
     };
-
 }]);
--- MyNodeServerFrontend/src/js/services/loginService.js
+++ MyNodeServerFrontend/src/js/services/loginService.js
@@ -1,13 +1,13 @@
-app.service('loginService', ['$http','$log' ,'Useraccount',function ($http,$log,Useraccount) {
+app.service('loginService', ['$http','$log' ,'Useraccount','sqlService',function ($http,$log,Useraccount,sqlService) {
     var self = this;
     self.userlevel = 'User';
-    self.isAuthenticated = function(username,password,salt){
-        if(username == 'schafezp' || username == 'zamanmm'){
-            // for now only permit two users
-            return true;
-        }else{
-            return false;
-        };
-
+    self.isAuthenticated = function(username,password,salt,callback){
+        sqlService.getUserByUsername(username,function(data){
+            if(data.length == 1){
+                callback(true);
+            }else{
+                callback(false);
+            }
+        });
     };
 }]);
