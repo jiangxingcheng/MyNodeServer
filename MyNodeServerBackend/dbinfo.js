c7320f84fffbc8028a5917009d241d2814f4948a
Wesley Van Pelt <wesley@Wesleys-MacBook-Pro.local>
2016-05-06 18:44:25 -0400

    Improved dbinfo.js for n00bs

--- MyNodeServerBackend/dbinfo.js
+++ MyNodeServerBackend/dbinfo.js
@@ -1,5 +1,5 @@
 var massive = require("massive");
 // Uncomment this and change your information. This will be in the .gitignore
-var connectionString = "postgres://person:assword@localhost:5432/mynodeserver";
+var connectionString = "postgres://your_postgres_user:password@localhost:5432/mynodeserver";
 var massiveInstance = massive.connectSync({connectionString : connectionString});
 exports.dbInstance = massiveInstance;
