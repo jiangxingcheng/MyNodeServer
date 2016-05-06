1eeb6e000edb520d8ef04f50e765c5107e56df7e
Wesley Van Pelt <wesley@Wesleys-MacBook-Pro.local>
2016-05-06 19:52:09 -0400

    Started making functions

--- MyNodeServerBackend/db/createdb.sql
+++ MyNodeServerBackend/db/createdb.sql
@@ -9,12 +9,10 @@ create TYPE userLevel as ENUM('Admin', 'Mod', 'User');
 --CREATE TYPE path must be implemented
 
 ------------ Create Tables ------------
---User is a reserved word so we must another
---CREATE TABLE User(
-CREATE TABLE User_account(
---LEN changed to LENGTH
+CREATE TABLE UserAccount(
 Username username CHECK(LENGTH(Username) > 1),
 Password VARCHAR(32) CHECK(LENGTH(Password) > 7),
+Salt text, --Would be not null, but that makes static data hard...
 UserLevel userLevel NOT NULL,
 LastAccessDate TIMESTAMP NOT NULL,
 TimeOfCreation TIMESTAMP NOT NULL,
@@ -25,8 +23,8 @@ CREATE TABLE Friends(
 Username1 username NOT NULL,
 Username2 username NOT NULL,
 PRIMARY KEY(Username1, Username2),
-FOREIGN KEY(Username1) REFERENCES User_Account(Username),
-FOREIGN KEY(Username2) REFERENCES User_Account(Username)
+FOREIGN KEY(Username1) REFERENCES UserAccount(Username),
+FOREIGN KEY(Username2) REFERENCES UserAccount(Username)
 );
 ------ Forum Tables ------
 CREATE TABLE Directory(
@@ -34,14 +32,14 @@ DPath fullpath NOT NULL,
 ParentPath fullpath,
 Username username,
 PRIMARY KEY(DPath),
-FOREIGN KEY(Username) REFERENCES User_Account(Username)
+FOREIGN KEY(Username) REFERENCES UserAccount(Username)
 );
 CREATE TABLE UserPermitsDirectory(
 Username username NOT NULL,
 DPath fullpath NOT NULL,
 PermissionLevel permissionLevel NOT NULL,
 PRIMARY KEY(Username, DPath),
-FOREIGN KEY(Username) REFERENCES User_Account(Username),
+FOREIGN KEY(Username) REFERENCES UserAccount(Username),
 FOREIGN KEY(DPath) REFERENCES Directory(DPath)
 );
 CREATE TABLE File(
@@ -49,14 +47,14 @@ FPath fullpath NOT NULL,
 ParentPath fullpath,
 Username username,
 PRIMARY KEY(FPath),
-FOREIGN KEY(Username) REFERENCES User_account(Username)
+FOREIGN KEY(Username) REFERENCES UserAccount(Username)
 );
 CREATE TABLE UserPermitsFile(
 Username username NOT NULL,
 FPath fullpath NOT NULL,
 PermissionLevel permissionLevel NOT NULL,
 PRIMARY KEY(Username, FPath),
-FOREIGN KEY(Username) REFERENCES User_Account(Username),
+FOREIGN KEY(Username) REFERENCES UserAccount(Username),
 FOREIGN KEY(FPath) REFERENCES File(FPath)
 );
 CREATE TABLE Category(
@@ -71,14 +69,14 @@ Title title NOT NULL,
 Username username NOT NULL,
 TimeOfCreation TIMESTAMP NOT NULL,
 PRIMARY KEY(Title),
-FOREIGN KEY(Username) REFERENCES User_account(Username)
+FOREIGN KEY(Username) REFERENCES UserAccount(Username)
 );
 CREATE TABLE Comment(
 Username username NOT NULL,
 TimeOfCreation TIMESTAMP NOT NULL,
 Text text NOT NULL CHECK(LENGTH(text) > 0),
 PRIMARY KEY(Username, TimeOfCreation),
-FOREIGN KEY(Username) REFERENCES User_account(Username)
+FOREIGN KEY(Username) REFERENCES UserAccount(Username)
 );
 CREATE TABLE CategoryHasThread(
 CTitle title NOT NULL,
@@ -93,7 +91,7 @@ TimeOfCreation TIMESTAMP NOT NULL,
 TTitle title NOT NULL,
 Text text NOT NULL CHECK(LENGTH(text) > 0),
 PRIMARY KEY(Username, TimeOfCreation),
-FOREIGN KEY(Username) REFERENCES User_account(Username),
+FOREIGN KEY(Username) REFERENCES UserAccount(Username),
 FOREIGN KEY(TTitle) REFERENCES Thread(Title)
 );
 CREATE TABLE DirectoryComment(
