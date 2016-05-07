7276c93e5e91acad0063de1a717e8e4437902993
Wesley Van Pelt <wesley@Wesleys-MacBook-Pro.local>
2016-05-06 22:12:30 -0400

    Some login and create user sprocs done

--- MyNodeServerBackend/db/createdb.sql
+++ MyNodeServerBackend/db/createdb.sql
@@ -1,7 +1,5 @@
 CREATE DOMAIN username VARCHAR(16) NOT NULL;
--- name was renamed to title
 CREATE DOMAIN title VARCHAR(64) NOT NULL;
---path was renamed to fullpath
 CREATE DOMAIN fullpath TEXT NOT NULL;
 
 CREATE TYPE permissionLevel AS ENUM('r', 'w', 'rw');
@@ -10,94 +8,104 @@ create TYPE userLevel as ENUM('Admin', 'Mod', 'User');
 
 ------------ Create Tables ------------
 CREATE TABLE UserAccount(
-Username username CHECK(LENGTH(Username) > 1),
-Password VARCHAR(32) CHECK(LENGTH(Password) > 7),
-Salt text, --Would be not null, but that makes static data hard...
-UserLevel userLevel NOT NULL,
-LastAccessDate TIMESTAMP NOT NULL,
-TimeOfCreation TIMESTAMP NOT NULL,
-PRIMARY KEY(Username)
+	Username username CHECK(LENGTH(Username) > 1),
+	Password TEXT CHECK(LENGTH(Password) > 7),
+	Salt TEXT, --Would be not null, but that makes static data hard...
+	UserLevel userLevel NOT NULL,
+	LastAccessDate TIMESTAMP NOT NULL,
+	TimeOfCreation TIMESTAMP NOT NULL,
+	PRIMARY KEY(Username)
 );
 
 CREATE TABLE Friends(
-Username1 username NOT NULL,
-Username2 username NOT NULL,
-PRIMARY KEY(Username1, Username2),
-FOREIGN KEY(Username1) REFERENCES UserAccount(Username),
-FOREIGN KEY(Username2) REFERENCES UserAccount(Username)
+	Username1 username NOT NULL,
+	Username2 username NOT NULL,
+	PRIMARY KEY(Username1, Username2),
+	FOREIGN KEY(Username1) REFERENCES UserAccount(Username),
+	FOREIGN KEY(Username2) REFERENCES UserAccount(Username)
 );
------- Forum Tables ------
+
 CREATE TABLE Directory(
-DPath fullpath NOT NULL,
-ParentPath fullpath,
-Username username,
-PRIMARY KEY(DPath),
-FOREIGN KEY(Username) REFERENCES UserAccount(Username)
+	DPath fullpath NOT NULL,
+	ParentPath fullpath,
+	Username username,
+	PRIMARY KEY(DPath),
+	FOREIGN KEY(Username) REFERENCES UserAccount(Username)
 );
+
 CREATE TABLE UserPermitsDirectory(
-Username username NOT NULL,
-DPath fullpath NOT NULL,
-PermissionLevel permissionLevel NOT NULL,
-PRIMARY KEY(Username, DPath),
-FOREIGN KEY(Username) REFERENCES UserAccount(Username),
-FOREIGN KEY(DPath) REFERENCES Directory(DPath)
+	Username username NOT NULL,
+	DPath fullpath NOT NULL,
+	PermissionLevel permissionLevel NOT NULL,
+	PRIMARY KEY(Username, DPath),
+	FOREIGN KEY(Username) REFERENCES UserAccount(Username),
+	FOREIGN KEY(DPath) REFERENCES Directory(DPath)
 );
+
 CREATE TABLE File(
-FPath fullpath NOT NULL,
-ParentPath fullpath,
-Username username,
-PRIMARY KEY(FPath),
-FOREIGN KEY(Username) REFERENCES UserAccount(Username)
+	FPath fullpath NOT NULL,
+	ParentPath fullpath,
+	Username username,
+	PRIMARY KEY(FPath),
+	FOREIGN KEY(Username) REFERENCES UserAccount(Username)
 );
+
 CREATE TABLE UserPermitsFile(
-Username username NOT NULL,
-FPath fullpath NOT NULL,
-PermissionLevel permissionLevel NOT NULL,
-PRIMARY KEY(Username, FPath),
-FOREIGN KEY(Username) REFERENCES UserAccount(Username),
-FOREIGN KEY(FPath) REFERENCES File(FPath)
+	Username username NOT NULL,
+	FPath fullpath NOT NULL,
+	PermissionLevel permissionLevel NOT NULL,
+	PRIMARY KEY(Username, FPath),
+	FOREIGN KEY(Username) REFERENCES UserAccount(Username),
+	FOREIGN KEY(FPath) REFERENCES File(FPath)
 );
+
+------ Forum Tables ------
 CREATE TABLE Category(
-Title title NOT NULL,
-Username username NOT NULL,
-TimeOfCreation TIMESTAMP NOT NULL,
-PRIMARY KEY(Title)
+	Title title NOT NULL,
+	Username username NOT NULL,
+	TimeOfCreation TIMESTAMP NOT NULL,
+	LogoPath TEXT,
+	PRIMARY KEY(Title)
 );
 
 CREATE TABLE Thread(
-Title title NOT NULL,
-Username username NOT NULL,
-TimeOfCreation TIMESTAMP NOT NULL,
-PRIMARY KEY(Title),
-FOREIGN KEY(Username) REFERENCES UserAccount(Username)
+	Title title NOT NULL,
+	Username username NOT NULL,
+	TimeOfCreation TIMESTAMP NOT NULL,
+	PRIMARY KEY(Title),
+	FOREIGN KEY(Username) REFERENCES UserAccount(Username)
 );
+
 CREATE TABLE Comment(
-Username username NOT NULL,
-TimeOfCreation TIMESTAMP NOT NULL,
-Text text NOT NULL CHECK(LENGTH(text) > 0),
-PRIMARY KEY(Username, TimeOfCreation),
-FOREIGN KEY(Username) REFERENCES UserAccount(Username)
+	Username username NOT NULL,
+	TimeOfCreation TIMESTAMP NOT NULL,
+	Text text NOT NULL CHECK(LENGTH(text) > 0),
+	PRIMARY KEY(Username, TimeOfCreation),
+	FOREIGN KEY(Username) REFERENCES UserAccount(Username)
 );
+
 CREATE TABLE CategoryHasThread(
-CTitle title NOT NULL,
-TTitle title NOT NULL,
-PRIMARY KEY(CTitle,TTitle),
-FOREIGN KEY(CTitle) REFERENCES Category(Title),
-FOREIGN KEY(TTitle) REFERENCES Thread(Title)
+	CTitle title NOT NULL,
+	TTitle title NOT NULL,
+	PRIMARY KEY(CTitle,TTitle),
+	FOREIGN KEY(CTitle) REFERENCES Category(Title),
+	FOREIGN KEY(TTitle) REFERENCES Thread(Title)
 );
+
 CREATE TABLE ThreadComment(
-Username username NOT NULL,
-TimeOfCreation TIMESTAMP NOT NULL,
-TTitle title NOT NULL,
-Text text NOT NULL CHECK(LENGTH(text) > 0),
-PRIMARY KEY(Username, TimeOfCreation),
-FOREIGN KEY(Username) REFERENCES UserAccount(Username),
-FOREIGN KEY(TTitle) REFERENCES Thread(Title)
+	Username username NOT NULL,
+	TimeOfCreation TIMESTAMP NOT NULL,
+	TTitle title NOT NULL,
+	Text text NOT NULL CHECK(LENGTH(text) > 0),
+	PRIMARY KEY(Username, TimeOfCreation),
+	FOREIGN KEY(Username) REFERENCES UserAccount(Username),
+	FOREIGN KEY(TTitle) REFERENCES Thread(Title)
 );
+
 CREATE TABLE DirectoryComment(
-Username username NOT NULL,
-TimeOfCreation TIMESTAMP NOT NULL,
-DPath fullpath NOT NULL,
-PRIMARY KEY(Username, TimeOfCreation),
-FOREIGN KEY(DPath) REFERENCES Directory(DPath)
+	Username username NOT NULL,
+	TimeOfCreation TIMESTAMP NOT NULL,
+	DPath fullpath NOT NULL,
+	PRIMARY KEY(Username, TimeOfCreation),
+	FOREIGN KEY(DPath) REFERENCES Directory(DPath)
 );
