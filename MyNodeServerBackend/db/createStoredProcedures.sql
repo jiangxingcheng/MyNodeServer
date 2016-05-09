2b10dd8911d9ad91a32694620770ccf54d4ef706
Wesley Van Pelt <wesley@Wesleys-MacBook-Pro.local>
2016-05-09 02:14:21 -0400

    Added some documentation

--- MyNodeServerBackend/db/createStoredProcedures.sql
+++ MyNodeServerBackend/db/createStoredProcedures.sql
@@ -1,3 +1,45 @@
+-- This file contains all of the functions to be created, only these should be used when interacting with the database.
+-- It contains the following:
+--
+-- User Management:
+--		createUser(uname TEXT, upassword TEXT) RETURNS VOID
+--		loginUser(uname TEXT, upassword TEXT) RETURNS TEXT
+--		userCount() RETURNS bigint
+--		getAllUsers() RETURNS TABLE(Username username, UserLevel VARCHAR(1), LastAccessDate TIMESTAMP, TimeOfCreation TIMESTAMP)
+--		findUser(uname TEXT) RETURNS TABLE(Username username, UserLevel VARCHAR(1), LastAccessDate TIMESTAMP, TimeOfCreation TIMESTAMP)
+--		removeUser(uname TEXT) RETURNS VOID
+--		setUserLevel(uname TEXT, newLevel VARCHAR(1)) RETURNS VOID
+--		addFriend
+--		removeFriend
+--		getFriends
+--
+-- File Management:
+--		ls(parentDir fullpath) RETURNS TABLE(itemPath fullpath)
+--		touch
+--		mkdir
+--		rm
+--		rm-r
+--		chmod
+--
+-- Forum Management:
+--		createCategory(cname title, uname TEXT, pathOfLogo TEXT) RETURNS VOID
+--		createThread(tname title, uname TEXT, threadCategory title, textBody TEXT) RETURNS VOID
+--		createThreadComment(uname TEXT, threadName title, userText TEXT) RETURNS VOID
+--		createDirComment
+--		createFileComment
+--		getAllCategories() RETURNS TABLE(title title, Username username, timeofCreation TIMESTAMP)
+--		getThreadsInCategory(categoryName title) RETURNS TABLE(Title title, Username username, timeofCreation TIMESTAMP)
+--		getThreadComments
+--		getDirComments
+--		getFileComments
+--
+-- Created by Wesley Van Pelt on 2016-05-06
+-- Last modified by Wesley Van Pelt on 2016-05-09
+--
+--------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------- User Management --------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------
+
 -- Takes the new username and password to salt and hash the password
 CREATE OR REPLACE FUNCTION createUser(uname TEXT, upassword TEXT) RETURNS VOID AS $$
 	DECLARE salt TEXT;
@@ -56,12 +98,36 @@ CREATE OR REPLACE FUNCTION setUserLevel(uname TEXT, newLevel VARCHAR(1)) RETURNS
 		UPDATE UserAccount SET UserLevel = newLevel WHERE username = uname;
 	END; $$ LANGUAGE plpgsql;
 
+-- addFriend
+
+-- removeFriend
+
+-- getFriends
+
+--------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------- File Management --------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------
+
 -- Get all files/subdirs in a dir
-CREATE OR REPLACE FUNCTION getSubStuff(parentDir fullpath) RETURNS TABLE(itemPath fullpath) AS $$
+CREATE OR REPLACE FUNCTION ls(parentDir fullpath) RETURNS TABLE(itemPath fullpath) AS $$
 	BEGIN
 		RETURN QUERY (SELECT DPath AS itemPath INTO dirs FROM Directory WHERE ParentPath=parentDir)
 					UNION (SELECT FPath AS itemPath INTO dirs FROM File WHERE ParentPath=parentDir);
 	END; $$ LANGUAGE plpgsql;
+
+-- touch
+
+-- mkdir
+
+-- rm
+
+-- rm-r
+
+-- chmod
+
+--------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------- Forum Management -------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------
 
 -- Create category
 CREATE OR REPLACE FUNCTION createCategory(cname title, uname TEXT, pathOfLogo TEXT) RETURNS VOID AS $$
@@ -75,16 +141,20 @@ CREATE OR REPLACE FUNCTION getAllCategories() RETURNS TABLE(title title, Usernam
 		RETURN QUERY SELECT c.CTitle AS title, c.Username, c.TimeOfCreation FROM Category c;
 	END; $$ LANGUAGE plpgsql;
 
+-- Create thread
+CREATE OR REPLACE FUNCTION createThread(tname title, uname TEXT, threadCategory title, textBody TEXT) RETURNS VOID AS $$
+	BEGIN
+	INSERT INTO Thread values(tname, uname, threadCategory, current_timestamp, textBody);
+	END; $$ LANGUAGE plpgsql;
+
+-- createDirComment
+
+-- createFileComment
+
 -- Get all threads in a category
 CREATE OR REPLACE FUNCTION getThreadsInCategory(categoryName title) RETURNS TABLE(Title title, Username username, timeofCreation TIMESTAMP) AS $$
 	BEGIN
 		RETURN QUERY SELECT t.TTitle as Title, t.Username, t.TimeOfCreation FROM Thread t WHERE CTitle=categoryName;
-	END; $$ LANGUAGE plpgsql;
-
--- Create thread
-CREATE OR REPLACE FUNCTION createThread(tname title, uname TEXT, threadCategory title, textBody TEXT) RETURNS VOID AS $$
-	BEGIN
-		INSERT INTO Thread values(tname, uname, threadCategory, current_timestamp, textBody);
 	END; $$ LANGUAGE plpgsql;
 
 -- Create thread comment
@@ -92,3 +162,9 @@ CREATE OR REPLACE FUNCTION createThreadComment(uname TEXT, threadName title, use
 	BEGIN
 		INSERT INTO ThreadComment values(uname, current_timestamp, threadName, userText);
 	END; $$ LANGUAGE plpgsql;
+
+-- getThreadComments
+
+-- getDirComments
+
+-- getFileComments
