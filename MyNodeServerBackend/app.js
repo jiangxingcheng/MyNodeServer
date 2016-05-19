86ba8b20ec2929e54340b09c9cacb22a53bcceaa
Zachary Schafer <schafezp@rose-hulman.edu>
2016-05-19 02:07:26 -0400

    Add middleware that allows CORS for DELETE for testing purposes

--- MyNodeServerBackend/app.js
+++ MyNodeServerBackend/app.js
@@ -12,6 +12,12 @@ app.set('db',dbInstance);
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //res.header("Access-Control-Allow-Methods","GET","POST","PUT","DELETE","OPTIONS");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header('Access-Control-Allow-Credentials', true);
+
+    res.header('Access-Control-Allow-Headers: X-Requested-With');
     next();
 });
 
