9b4954dba9c7346b2e464762478b596889eaf692
Zachary Schafer <schafezp@rose-hulman.edu>
2016-05-19 22:39:32 -0400

    Make clicking on file navigate to subdirectory

--- MyNodeServerFrontend/src/js/controllers/FilesCtrl.js
+++ MyNodeServerFrontend/src/js/controllers/FilesCtrl.js
@@ -1,12 +1,22 @@
 app.controller('FilesCtrl', ['$scope', '$location', '$log', 'sqlService', '$timeout','Files', function ($scope, $location, $log, sqlService, $timeout,Files) {
     $scope.contents = [];
     $scope.username = sqlService.username;
-    sqlService.ls($scope.username,'/home/'+ $scope.username + '/',function(data){
+    $scope.homepath = '/home/'+ $scope.username + '/';
+    $log.log($scope.homepath);
+    $scope.currentpath = $scope.homepath;
+    sqlService.ls($scope.username,$scope.currentpath,function(data){
         $scope.contents = data;
     });
 
     $scope.openContent = function(element){
         $log.log('Element');
         $log.log(element);
+        $scope.currentpath = $scope.currentpath + element.filepath[element.filepath.length-1] + '/';
+        $log.log($scope.currentpath);
+        sqlService.ls($scope.username,$scope.currentpath,function(data){
+            $scope.contents = data;
+            $log.log('contents is');
+            $log.log($scope.contents);
+        });
     };
 }]);
