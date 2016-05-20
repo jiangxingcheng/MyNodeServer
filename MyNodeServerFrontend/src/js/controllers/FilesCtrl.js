7fc41164ddcdfc9ce5c276eaa09ac679e737ff32
Zachary Schafer <schafezp@rose-hulman.edu>
2016-05-19 22:01:34 -0400

    Directory shows user's home by default

--- MyNodeServerFrontend/src/js/controllers/FilesCtrl.js
+++ MyNodeServerFrontend/src/js/controllers/FilesCtrl.js
@@ -1,89 +1,12 @@
 app.controller('FilesCtrl', ['$scope', '$location', '$log', 'sqlService', '$timeout','Files', function ($scope, $location, $log, sqlService, $timeout,Files) {
-    $scope.results = [];
-    sqlService.ls('schafezp','/home/schafezp/',function(data){
-        $scope.results = data;
+    $scope.contents = [];
+    $scope.username = sqlService.username;
+    sqlService.ls($scope.username,'/home/'+ $scope.username + '/',function(data){
+        $scope.contents = data;
     });
-    $scope.contents = [
-        {
-            type: 'file',
-            name: 'penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'gay sex'
-        },
-        {
-            type: 'file',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'file',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        },
-        {
-            type: 'file',
-            name: 'lots of penis'
-        },
-        {
-            type: 'dir',
-            name: 'lots of penis'
-        }
-    ];
+
     $scope.openContent = function(element){
         $log.log('Element');
         $log.log(element);
     };
-    $log.log($scope.contents[0].type);
 }]);
